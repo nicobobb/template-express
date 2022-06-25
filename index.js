0dfe25ea275de1bca3da5c6ce1441ea4e66592d7
Nicolás Bobb <nicobobb@gmail.com>
2022-06-25 16:34:28 -0300

    Ejemplo respuesta Json desde express

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hola desde mi App con Express.js!'));
 
+app.get('/productos', (req,res) => { 
+    res.json({
+        nombre: 'Monitor',
+        precio: '$500',
+        cantidad: '1'
+    });
+});
+
 app.listen(port, () => console.log(`Escuchando al puerto ${port}!`));
