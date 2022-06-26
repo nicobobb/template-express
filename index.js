eab1d54c6f1ddcff1b9a709d311a5eed86eba904
Nicolás Bobb <nicobobb@gmail.com>
2022-06-26 11:52:29 -0300

    Parámetros query

--- index.js
+++ index.js
@@ -6,11 +6,51 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hola desde mi App con Express.js!'));
 
 app.get('/productos', (req,res) => { 
+    res.json([{
+        nombre: 'Monitor',
+        precio: '$500',
+        cantidad: '1'
+    }, {
+        nombre: 'Teclado',
+        precio: '$100',
+        cantidad: '2'
+    }]);
+});
+
+app.get('/productos/:id', (req, res) => {
+    const { id } =req.params;
     res.json({
+        id: id,
         nombre: 'Monitor',
         precio: '$500',
         cantidad: '1'
     });
-});
+})
+
+app.get('/categorias/:categoriaId/productos/:productoId', (req, res) => {
+    const { categoriaId, productoId } = req.params;
+    res.json({
+        categoriaId,
+        productoId
+    
+    });
+
+})
+
+// http://localhost:3000/usuarios?limit=10&offset=200
+app.get('/usuarios', (req,res) => {
+    const { limit, offset } = req.query;
+    if (limit && offset) {
+        res.json({
+            limit,
+            offset
+        });
+    } else {
+        res.json({
+            limit: 'No se ha especificado limit',
+            offset: 'No se ha especificado offset'
+        });
+    }
+})
 
 app.listen(port, () => console.log(`Escuchando al puerto ${port}!`));
